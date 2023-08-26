e1bb982be2a0eb146c9f53e31af844a888255f25
Russ <russ3750@gmail.com>
2023-08-26 18:48:18 -0500

    updated messages at end of game

--- score.js
+++ score.js
@@ -29,3 +29,29 @@ saveHighScore = e => {
     window.location.assign('achievers.html')
 
 }
+
+if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+	localStorage.setItem("mostRecentScore", score);
+
+	let finalMessage;
+	if (score <= 25) {
+		finalMessage = "Hey, better luck next time!";
+	} else if (score > 25 && score <= 50) {
+		finalMessage = "Not bad, you're getting there!";
+	} else if (score > 50 && score <= 75) {
+		finalMessage = "Nice, you're pretty smart!";
+	} else {
+		finalMessage = "Wow, you're a quiz genius!";
+	}
+
+	
+	document.getElementById("finalMessage").innerText = finalMessage;
+
+	return window.location.assign("score.html");
+}
+
+window.onload = function () {
+	const mostRecentScore = localStorage.getItem("mostRecentScore");
+	document.getElementById("finalScore").innerText = mostRecentScore;
+};
+
